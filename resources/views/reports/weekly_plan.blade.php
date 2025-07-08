9103f4368f8a7d67160bd4cf8ba974c38bb2aae3
DseCasis <dse.casis@yavirac.edu.ec>
2025-07-08 13:57:16 -0500

    feat: Arreglo de bugs
    - Se soluciono el error que no permitia la correcta muestra del iapoyo logistico en el reporte semanal de actividades

--- resources/views/reports/weekly_plan.blade.php
+++ resources/views/reports/weekly_plan.blade.php
@@ -280,9 +280,9 @@
             <td>
                 @forelse ($currentDayActivities as $activity)
                     <div class="activity-details">
-                        @if (!empty($activity->logisticSupports) && is_iterable($activity->logisticSupports) && $activity->logisticSupports->isNotEmpty())
+                        @if (!empty($activity->logisticSupportUsers) && is_iterable($activity->logisticSupportUsers) && $activity->logisticSupportUsers->isNotEmpty())
                             <ul>
-                                @foreach ($activity->logisticSupports as $support)
+                                @foreach ($activity->logisticSupportUsers as $support)
                                     <li>{{ $support->name }}</li>
                                 @endforeach
                             </ul>
