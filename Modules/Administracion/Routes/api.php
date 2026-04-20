8c4f001d5723ac1398416d2ef81d4aebdfc58ab9
DannyCasis <danny.casis@iniap.gob.ec>
2026-04-20 20:04:25 +0000

    feature (fix): Se agregaron las rutas eliminadas del POA VISIBILITY que no permitian asignar la vista a las unidades

--- Modules/Administracion/Routes/api.php
+++ Modules/Administracion/Routes/api.php
@@ -2,21 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Administracion\Http\Controllers\DispatchController;
+use Modules\Administracion\Http\Controllers\PoaVisibilityController;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |*/
-Route::prefix('administracion')->middleware(['auth:api'])->group(function () {
+Route::prefix('administracion')->middleware(['auth:api'])->group(callback: function () {
 
-    // 1. Obtener la lista de solicitudes (Kanban de Despachos)
     Route::get('/dispatches', [DispatchController::class, 'index']);
 
-    // 2. Procesar un despacho (El administrador confirma cantidades y entrega)
     Route::post('/dispatches', [DispatchController::class, 'store']);
 
-    // 3. Ver detalle histórico de un despacho específico (opcional pero recomendado)
     Route::get('/dispatches/{id}', [DispatchController::class, 'show']);
+
+    Route::get('/poa-visibility', [PoaVisibilityController::class, 'index']);
+    Route::post('/poa-visibility', [PoaVisibilityController::class, 'sync']);
 
 });
